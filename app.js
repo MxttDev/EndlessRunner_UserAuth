fcaad32e53e848c0a3ed49c9ba0cd11e41589fbc
Matt Stott <mattyrstott@gmail.com>
2022-02-20 01:41:00 +0000

    1.0
    sd

--- app.js
+++ app.js
@@ -6,11 +6,9 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const User = require("./models/user")
-const Store = require('./models/store')
-const Leaderboard = require('./models/leaderboard')
 
 //Connect to mongo database
-mongoose.connect("mongodb+srv://admin:zraJhUoTCuZi2pxkTukVMANej7qNWNMo6kIxZTsv0PcGhmPLJnAWOWm6rf0xn4Af3xxxUwNrg1FE0GKqqeBbEluzu80NowcWLdHeQXzfcQ8bTIFs4gOZh2WYz4oQYTzO@target.xqtl8.mongodb.net/database?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://admin:zraJhUoTCuZi2pxkTukVMANej7qNWNMo6kIxZTsv0PcGhmPLJnAWOWm6rf0xn4Af3xxxUwNrg1FE0GKqqeBbEluzu80NowcWLdHeQXzfcQ8bTIFs4gOZh2WYz4oQYTzO@userstorage.v7p3x.mongodb.net/storage?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
@@ -19,10 +17,8 @@ mongoose.connect("mongodb+srv://admin:zraJhUoTCuZi2pxkTukVMANej7qNWNMo6kIxZTsv0P
   console.log("Error connecting to Mongo", err);
 });
 
-require('./routes/users/steamID')(app);
+require('./routes/users/getSpecificUsername')(app);
 require('./routes/users/users')(app);
-require('./routes/shop/getShopItems')(app);
-require('./routes/leaderboard/top')(app);
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -30,46 +26,15 @@ app.use(bodyParser.json());
 
 app.post("/users/create", (req, res) => {
   const user = new User({
-    name: req.body.name,
-    steamId: req.body.steamId,
-    totalKills: req.body.totalKills,
-    totalDeaths: req.body.totalDeaths,
-    totalWins: req.body.totalWins,
-    totalLoss: req.body.totalLoss,
-    level: req.body.level,
-    experience: req.body.experience,
-    items: req.body.items
+    username: req.body.username,
+    password: req.body.password,
   })
-	user.save(function (err, user) {
-    if (err) { return next(err) }
-    res.status(201).json(user)
-  })
+	user.save()
 })
 
-app.post("/store/add", (req, res) => {
-  const store = new Store({
-    productName: req.body.productName,
-    description: req.body.description,
-  })
-	store.save(function (err, store) {
-    if (err) { return res.send(err) }
-    res.status(201).json(store)
-  })
-})
 
-app.post("/leaderboard/add", (req, res) => {
-  const leaderboard = new Leaderboard({
-    playerName: req.body.playerName,
-    Rank: req.body.Rank,
-    Wins: req.body.Wins,
-    MatchPlayed: req.body.MatchPlayed,
-  })
-	leaderboard.save(function (err, leaderboard) {
-    if (err) { return res.send(err) }
-    res.status(201).json(leaderboard)
-  })
-})
+const PORT = process.env.PORT || 3000;
 
-app.listen("3000", ()=> {
-	console.log('listening on port 3000')
-})
+app.listen(PORT || 3000, () => {
+  console.log(`Server running on port ${PORT}`);
+});
--- models/user.js
+++ models/user.js
@@ -1,41 +1,13 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  steamId: {
-    type: Number,
+  username: {
+    type: String,
     required: true,
     unique: true,
   },
-  name: {
+  password: {
     type: String,
-    required: true,
-  },
-  totalKills: {
-    type: Number,
-    required: true,
-  },
-  totalDeaths: {
-    type: Number,
-    required: true,
-  },
-  totalWins: {
-    type: Number,
-    required: true,
-  },
-  totalLoss: {
-    type: Number,
-    required: true,
-  },
-  level: {
-    type: Number,
-    required: true,
-  },
-  experience: {
-    type: Number,
-    required: true,
-  },
-  items: {
-    type: [String],
     required: true,
   }
 });
