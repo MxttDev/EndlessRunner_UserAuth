34d0b8ee7ee91f8e2b4d13d2f896664107742358
Matt Stott <mattyrstott@gmail.com>
2022-03-20 13:49:41 +0000

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 
 const mongoose = require("mongoose");
 
-const Data = require("./models/users")
+const Data = require("./models/data")
 
 
 //Connect to mongo database
