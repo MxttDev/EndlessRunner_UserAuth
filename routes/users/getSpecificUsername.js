6b8f33da87088729bd7d8876042034b90aaf3aa7
Matt Stott <mattyrstott@gmail.com>
2022-03-20 15:25:34 +0000

    Update getSpecificUsername.js

--- routes/users/getSpecificUsername.js
+++ routes/users/getSpecificUsername.js
@@ -1,11 +1,11 @@
 let mongoose = require("mongoose");
 
-const User = mongoose.model('User');
+const Data = mongoose.model('Data');
 
 
 module.exports = function(app) {
     app.get("/users/:username", (req, res) => {
-        User.find({username: req.params.username.toLowerCase()}, '-_id').sort().limit(1).exec(function(err, users) {
+      Data.find({username: req.params.username.toLowerCase()}, '-_id').sort().limit(1).exec(function(err, users) {
             var user = users[0]
 
             if(err) return res.json({error: true});
